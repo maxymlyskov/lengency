dbcead6743d64100a35b8a9795f6bde8c934f6d4
maxymlyskov <raypember02@gmail.com>
2023-02-23 13:59:37 +0200

    Started doing navbar on footer.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 function Layout({children}) {
+    const menuItems = [
+        {title: 'Home' , path: '/', icon: ''},
+        {title: 'About' , path: '/about', icon: ''},
+        {title: 'Clients' , path: '/clients', icon: ''},
+        {title: 'Contact' , path: '/contact', icon: ''},
+    ]
   return (
     <div>
-        <div className="content">{children}</div>
-        <div className="footer"><h1>Footer</h1></div>
+        {/* Content */}
+        <div className="">{children}</div>
+
+        {/* Footer */}
+        <div className="fixed bottom-10 left-0 right-0 text-center w-full">
+            <div className="flex w-full justify-center">
+                {menuItems.map((item, i)=><div className={`px-20 bg-primary py-5 ${i===0 && 'rounded-l'} ${i===menuItems.length-1 && 'rounded-r'}`}>
+                    <Link to={`${item.path}`} className="text-secondary text-xl">{item.title}</Link> 
+                </div>)}
+            </div>
+        </div>
     </div>
   )
 }
