37a8a0e3b3d76002b8933f4f40549704ed4f129f
maxymlyskov <raypember02@gmail.com>
2023-02-28 16:45:17 +0200

    Made WhatWeDo section.

--- src/pages/About/WhatWeDo.jsx
+++ src/pages/About/WhatWeDo.jsx
@@ -25,7 +25,17 @@ function WhatWeDo() {
     ]
   return (
     <div className='mt-20'>
-        <h1 className='text-secondary text-8xl text-center'>What We Do ?</h1>
+        <h1 className='text-secondary text-8xl text-center z-20'>What We Do ?</h1>
+        <div className="h-96 w-full bg-primary mt-12"></div>
+        <div className="grid grid-cols-4 gap-10 mx-32 -mt-48">
+            {items.map(item=>{
+                return <div className="p-5 bg-white border shadow flex flex-col space-y-5 items-center transform hover:scale-105 duration-300">
+                    <img src={item.image} className="h-20 w-20" alt={item.title}/>
+                    <h1 className='text-2xl'>{item.title}</h1>
+                    <p className='text-gray-600 text-md'>{item.description}</p>
+                </div>
+            })}
+        </div>
     </div>
   )
 }
