441667c7a35d7059680e14c7f86c920e1a417a7c
maxymlyskov <raypember02@gmail.com>
2023-03-01 16:51:57 +0200

    Made ContactForm section.

--- src/pages/Contact/ContactForm.jsx
+++ src/pages/Contact/ContactForm.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 function ContactForm() {
   return (
-    <div>ContactForm</div>
+    <div>
+        <form className='flex flex-col space-y-10'>
+            <h1 className='text-4xl font-semibold text-primary mb-5'>Send a Message</h1>
+
+            <div className="flex space-x-10">
+                <input type="text" placeholder='First name *' className='py-3 px-5 bg-blue-50 focus:outline-none rounded-lg text-gray-500 w-full' />
+                <input type="text" placeholder='Last name *' className='py-3 px-5 bg-blue-50 focus:outline-none rounded-lg text-gray-500 w-full' />
+            </div>
+            <div className="flex space-x-10">
+                <input type="text" placeholder='Email *' className='py-3 px-5 bg-blue-50 focus:outline-none rounded-lg text-gray-500 w-full' />
+                <input type="text" placeholder='Phone number' className='py-3 px-5 bg-blue-50 focus:outline-none rounded-lg text-gray-500 w-full' />
+            </div>
+            <div className="flex space-x-10">
+                <textarea rows={5} placeholder='Message' className='py-3 px-5 bg-blue-50 focus:outline-none rounded-lg text-gray-500 w-full' />
+            </div>
+            <div className="flex justify-end">
+            <button className='text-white bg-secondary px-5 py-3 rounded-xl max-w-max'>Send a Message</button>
+            </div>
+        </form>
+    </div>
   )
 }
 
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -6,7 +6,7 @@ import ContactForm from './ContactForm'
 function Contact() {
   return (
       <Layout>
-        <div className='grid grid-cols-2 px-32'>
+        <div className='grid grid-cols-2 px-32 gap-20 mt-32'>
           <Address/>
           <ContactForm/>
         </div>
