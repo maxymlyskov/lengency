d9c51e4d8ebbb8958a289a029a3998e72d46132e
maxymlyskov <raypember02@gmail.com>
2023-03-02 15:52:27 +0200

    Made footer responsive.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Link, useLocation} from 'react-router-dom'
+import {Link, useLocation, useNavigate} from 'react-router-dom'
 function Layout({children}) {
+    const navigate = useNavigate()
     const location = useLocation()
     const menuItems = [
         {title: 'Home' , path: '/', icon: 'ri-home-smile-line'},
@@ -14,21 +15,22 @@ function Layout({children}) {
         <div className="pb-44 sm:pb-20 overflow-x-hidden">{children}</div>
 
         {/* Footer */}
-        <div className="fixed bottom-10 left-0 right-0 text-center w-full">
+        <div className="fixed bottom-10 left-0 right-0 text-center w-full sm:bottom-0">
+
             <div className="flex w-full justify-center">
                 {menuItems.map((item, i)=>
-                <div key={item.title} className='flex flex-col justify-end items-center '>
-                {location.pathname ===item.path &&  (<div className='flex flex-col items-center'>
-                    <div className='h-5 w-10 bg-primary rounded-t-full'></div>
-                    <div className='h-5 w-20 bg-primary rounded-t-full text-center'>
-                        <i className={`${item.icon} text-xl text-white`}></i>
+                    <div key={item.title} className='flex flex-col justify-end items-center '>
+                    {location.pathname ===item.path &&  (<div className='flex flex-col items-center'>
+                        <div className='h-5 w-10 bg-primary rounded-t-full'></div>
+                        <div className='h-5 w-20 bg-primary rounded-t-full text-center'>
+                            <i className={`${item.icon} text-xl text-white`}></i>
+                        </div>
+                    </div>)}
+                    <div className={`px-20 sm:px-7   bg-primary py-5 ${i===0 && 'rounded-l'} ${i===menuItems.length-1 && 'rounded-r'} flex items-center justify-center space-x-2`}>
+                        {location.pathname !== item.path && <i onClick={()=>navigate(item.path)} className={`${item.icon} text-secondary text-xl`}></i>}
+                        <Link to={`${item.path}`} className={`text-secondary text-xl ${item.path!==location.pathname && 'sm:hidden'}`}>{item.title}</Link> 
                     </div>
-                </div>)}
-                <div className={`px-20 bg-primary py-5 ${i===0 && 'rounded-l'} ${i===menuItems.length-1 && 'rounded-r'} flex items-center justify-center space-x-2`}>
-                    {location.pathname !== item.path && <i className={`${item.icon} text-secondary text-xl`}></i>}
-                    <Link to={`${item.path}`} className="text-secondary text-xl">{item.title}</Link> 
-                </div>
-                </div>
+                    </div>
                 )}
             </div>
         </div>
